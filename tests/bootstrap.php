a109243d5e5da9c9dc65103a699057078a939651
Marshall Sorenson <marshall.sorenson@gmail.com>
2014-11-13 10:02:36 -0500

    significant build improvements, source files have been moved to new src dir

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,8 +9,28 @@ $PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'];
 // init global vars
 global $wpdb, $current_site, $current_blog, $wp_rewrite, $shortcode_tags, $wp;
 
+// default wp debug setting
+if ( false === defined( 'WP_DEBUG' ) ) {
+	define( 'WP_DEBUG', false );
+}
+
+// default wp cron setting
+if ( false === defined( 'DISABLE_WP_CRON' ) ) {
+	define( 'DISABLE_WP_CRON', true );
+}
+
+// default path to wordpress env
+if ( false === defined( 'WP_SDL_BOOTSTRAP_ENV' ) ) {
+	define( 'WP_SDL_BOOTSTRAP_ENV', '/var/www/html' );
+}
+
+// default path to src files
+if ( false === defined( 'WP_SDL_BOOTSTRAP_SRC' ) ) {
+	define( 'WP_SDL_BOOTSTRAP_SRC', getcwd() . '/src' );
+}
+
 // load wordpress
-require_once WP_TEST_LOAD_PATH;
+require_once WP_SDL_BOOTSTRAP_ENV . '/wp-load.php';
 
 // load wp-sdl
-require_once dirname(dirname(__FILE__)) . '/wp-sdl.php';
+require_once WP_SDL_BOOTSTRAP_SRC . '/wp-sdl.php';
